21e0a556c0ce64d4715d00397b567abb327532a3
Garrick S. Bodine <garrick.bodine@gmail.com>
2013-01-05 22:59:27 -0500

    Added Flash helper functionality so that it's a Bootstrap modal instead of page content in most cases. This is really what gsbodine/omeka-bootstrap#19 was about.

--- CrowdEd/views/helpers/Flash.php
+++ CrowdEd/views/helpers/Flash.php
@@ -5,17 +5,21 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
-require_once APP_DIR . '/views/helpers/Flash.php';
+class CrowdEd_View_Helper_Flash extends Zend_View_Helper_Abstract {
+    
+    private $_flashMessenger;
 
-class CrowdEd_View_Helper_Flash extends Omeka_View_Helper_Flash {
-    /*public function flash($flashIcon = null, $flashHeading = null) {
+    public function __construct() {
+        $this->_flashMessenger = Zend_Controller_Action_HelperBroker::getStaticHelper('FlashMessenger');
+    }
+    
+    public function flash() {
         $flashHtml = '';
-        if ($this->_flashMessenger->hasMessages()
-         || $this->_flashMessenger->hasCurrentMessages()) {
-            $flashHtml .= '<div id="flash" class="modal hide fade" role="dialog" tab-index="-1" aria-labelled-by="flashModal">';
+        if ($this->_flashMessenger->hasMessages() || $this->_flashMessenger->hasCurrentMessages()) {
+            $flashHtml .= '<div id="flash" class="modal show fade" role="dialog" tab-index="-1" aria-labelled-by="flashModal">';
             $flashHtml .= '<div class="modal-header">';
-            $flashHtml .= '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="flashModal">'. $flashIcon . ' ' . $flashHeading .'</h3></div>';
-            $flashHtml .= '<ul class="unstyled">';
+            $flashHtml .= '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h3><i class="icon-info-sign"></i> '. get_option('site_title') .'</h3></div>';
+            $flashHtml .= '<div class="modal-body"><ul class="unstyled">';
             foreach ($this->_flashMessenger->getMessages() as $status => $messages) {
                 foreach ($messages as $message) {
                     $flashHtml .= $this->_getListHtml($status, $message);
@@ -26,13 +30,18 @@ class CrowdEd_View_Helper_Flash extends Omeka_View_Helper_Flash {
                     $flashHtml .= $this->_getListHtml($status, $message);
                 }
             }
-            $flashHtml .= '</ul></div>';
+            $flashHtml .= '</ul></div><div class="modal-footer"><button type="button" class="btn" data-dismiss="modal">OK</button></div></div>';
         }
         $this->_flashMessenger->clearMessages();
         $this->_flashMessenger->clearCurrentMessages();
         return $flashHtml;
-    }*/
+    }
     
+    private function _getListHtml($status, $message) {
+        return '<li class="' . $this->view->escape($status) . '">' 
+            . $this->view->escape($message)
+            . '</li>';
+    }
 }
 
 ?>
